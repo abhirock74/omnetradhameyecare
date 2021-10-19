eb7edda9d356a8a1468b7a616a2eae64b5bffa7b
ani07ket <aniket.suvaidyam@gmail.com>
2021-10-19 13:34:42 +0530

    add php

--- mail.php
+++ mail.php
@@ -1,78 +1,17 @@
 <?php
-
-$errorMSG = "";
-
-// NAME
-if (empty($_POST["name"])) {
-    $errorMSG = "Name is required ";
-} else {
-    $name = $_POST["name"];
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$phone= $_post['phone']
+$to = "aniketsinghne@gmail.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@yoursite.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
 }
-
-// EMAIL
-if (empty($_POST["email"])) {
-    $errorMSG .= "Email is required ";
-} else {
-    $email = $_POST["email"];
-}
-
-// MSG Guest
-if (empty($_POST["date"])) {
-    $errorMSG .= "Subject is required ";
-} else {
-    $date = $_POST["date"];
-}
-
-
-// MSG Event
-if (empty($_POST["time"])) {
-    $errorMSG .= "Subject is required ";
-} else {
-    $time = $_POST["time"];
-}
-
-
-// MESSAGE
-if (empty($_POST["message"])) {
-    $errorMSG .= "Message is required ";
-} else {
-    $message = $_POST["message"];
-}
-
-
-$EmailTo = "aniketsinghne@gmail.com";
-$Subject = "New Message Received";
-
-// prepare email body text
-$Body = "";
-$Body .= "Name: ";
-$Body .= $name;
-$Body .= "\n";
-$Body .= "Email: ";
-$Body .= $email;
-$Body .= "\n";
-$Body .= "guest: ";
-$Body .= $date;
-$Body .= "\n";
-$Body .= "event: ";
-$Body .= $time;
-$Body .= "\n";
-$Body .= "Message: ";
-$Body .= $message;
-$Body .= "\n";
-
-// send email
-$success = mail($EmailTo, $Subject, $Body, "From:".$email);
-
-// redirect to success page
-if ($success && $errorMSG == ""){
-   echo "success";
-}else{
-    if($errorMSG == ""){
-        echo "Something went wrong :(";
-    } else {
-        echo $errorMSG;
-    }
-}
-
+//redirect
+header("Location:thankyou.html");
 ?>
